a52709db53648a1258191c94abbc19ed421cfaf8
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-07-07 13:11:02 -0400

    users controllers basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :destroy]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @characters = @user.characters
+    end
+
+
+    private
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 end
