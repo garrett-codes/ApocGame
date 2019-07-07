a289aac4dd762d0142ba184ca5df3ba90b20fac9
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-07 15:40:37 -0400

    adding the team branch to master

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.destroy_all
+
+Team.create(name: "team 1", win: false)
