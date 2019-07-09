1c8e74f5190631fe10609af53aeaf95b39d935dc
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-09 16:46:05 -0400

    added modified new cloumns to task

--- db/migrate/20190709203347_add_new_columns_to_task.rb
+++ db/migrate/20190709203347_add_new_columns_to_task.rb
@@ -1,6 +1,6 @@
 class AddNewColumnsToTask < ActiveRecord::Migration[5.2]
   def change
-    add_column :tasks, :name, :string
+    add_column :tasks, :name, :string, default: "And so it begins..."
     add_column :tasks, :situation, :string
     add_column :tasks, :choice1, :string
     add_column :tasks, :choice2, :string
