094806474933a716ae4efd4098043ff95c9341f0
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-10 17:45:55 -0400

    latest task to merge to master

--- db/migrate/20190709202639_add_task_columns_to_character.rb
+++
@@ -1,6 +0,0 @@
-class AddTaskColumnsToCharacter < ActiveRecord::Migration[5.2]
-  def change
-    add_column :characters, :completed_tasks, :integer, default: 0
-    add_column :characters, :remaining_tasks, :integer, default: 5
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2019_07_09_203347) do
     t.string "task_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "result1"
+    t.string "result2"
+    t.string "choice1"
+    t.string "choice2"
+    t.string "consequences"
     t.string "name", default: "And so it begins..."
     t.string "situation"
     t.string "correct", default: "Destroy the brain!"
